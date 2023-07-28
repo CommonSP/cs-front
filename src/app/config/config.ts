592534388a202a5a54cb5435928a2813c3ab6674
CommonSP <Evgeniy2000sa@yandex.ru>
2023-07-28 20:38:34 +0300

    ff

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -5,6 +5,6 @@ const baseUrl = environment.baseUrl
 export const PRODUCTS = baseUrl + '/api/products/'
 export const PRODUCT_IMAGE = baseUrl + '/api/images/product/'
 export const IMAGES = baseUrl + '/api/images/'
-export const SALE_IMAGE = baseUrl + '/api/image/sale/'
+export const SALE_IMAGE = baseUrl + '/api/imagess/sale/'
 export const USER = baseUrl + '/api/user/'
 export const SALES = baseUrl + '/api/sales/'
